fe0b1a977ed91727b5a8e6d8fdb9bf2e5e1b0324
Sefineh Tesfa <sefinehtesfa34@gmail.com>
2021-12-09 11:30:12 -0800

    counting sort algorithm from HackerRank

---
+++ countingSort.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    mx=max(arr)
+    if mx%2==0:
+        created=[0]*(mx+2)
+    else:
+        created=[0]*(mx+1)
+        
+    for i in range(len(arr)):
+        created[arr[i]]+=1
+    return created
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
