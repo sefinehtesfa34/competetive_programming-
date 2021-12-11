16a7fbd63ef1216d091d8d4d429ae9444e709cc7
Sefineh Tesfa <sefinehtesfa34@gmail.com>
2021-12-11 04:18:38 -0800

    insertion sort problem implementation

---
+++ insertionSort.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'insertionSort1' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY arr
+#
+
+def insertionSort1(n, arr):
+    val=arr[-1]
+    for j in range(len(arr)-1,0,-1):
+        if arr[j-1]<val:
+            arr[j]=val
+            for i in arr:
+                print(i,end=" ")
+            break
+        elif j==1:
+            arr[j]=arr[j-1]
+            arr[j-1]=val
+            for i in arr:
+                print(i,end=" ")
+            break
+        arr[j]=arr[j-1]
+        for i in arr:
+            print(i,end=" ")
+        print()
+    
+        
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    insertionSort1(n, arr)
