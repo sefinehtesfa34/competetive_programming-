2107b2a4921fec09e4695e11b6255d358585b05b
Sefineh Tesfa <sefinehtesfa34@gmail.com>
2021-12-19 05:44:02 -0800

    closest sum hackerRank

--- minMaxPairSum.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def minPairSum(self, nums: List[int]) -> int:
-        nums.sort()
-        l=[]
-        for i in range(len(nums)//2):
-            l.append(nums[i:i+1][0]+nums[len(nums)-1-i:len(nums)-i][0])
-        return max(l)
