64cb960476ef763642ab2d6de2d3a96f7fe8a0a3
Sefineh Tesfa <88246220+sefinehtesfa34@users.noreply.github.com>
2021-12-19 12:47:47 -0800

    Maximum Number of Coins You Can Get

--- maxCoin.py
+++ maxCoin.py
@@ -1,11 +1,16 @@
-piles=[2,4,1,2,7,8]
-val=0
-for i in range(len(piles)//3):
-    mx=max(piles)
-    piles.remove(mx)
-    mn=min(piles)
-    piles.remove(mn)
-    mx=max(piles)
-    piles.remove(mx)
-    val+=mx
-print(val)
+class Solution:
+    def maxCoins(self, piles: List[int]) -> int:
+        piles.sort(reverse=True)
+        val=0
+        for i in range(len(piles)):
+            if len(piles)>=3:
+                piles.pop()
+                piles.pop(0)
+                mxVal=piles.pop(0)
+                val+=mxVal
+            else:
+                break
+        return val
+            
+
+            
