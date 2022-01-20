ad2848447b6b2806c9cfc58327cdcd4ae169bc16
Sefineh Tesfa <88246220+sefinehtesfa34@users.noreply.github.com>
2022-01-20 04:45:06 -0800

    remove nth node from left in linked list

---
+++ Remove_NthNodeFromLeft.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        temp=head
+        counter=0
+        while temp:
+            counter+=1
+            temp=temp.next
+        if counter==1:
+            return None
+        rmvNode=counter-n
+        temp=head
+        if rmvNode==0:
+            head=temp.next
+            return head
+        while rmvNode>1:
+            temp=temp.next
+            rmvNode-=1
+        if temp.next:
+            temp.next=temp.next.next
+        return head
+            
+        
+        
+        
+        
