0cf45c8b87cd8f77363ff1179b4b3acf3f0d95fd
Sefineh Tesfa <88246220+sefinehtesfa34@users.noreply.github.com>
2022-02-22 04:49:44 -0800

    Top K Frequent Elements

---
+++ topKFrequentElements.py
@@ -0,0 +1,27 @@
+import heapq as hq
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        dic={}
+        output=[]
+        for i in nums:
+            if i not in dic:
+                dic[i]=1
+            else:
+                dic[i]+=1
+        values=sorted(dic.items(),key=lambda x:x[1])
+        for i in range(k):
+            output.append(values[-1-i][0])
+        return output
+    
+    
+    
+    
+    
+#         negValues=list(map(self.negative,nums))
+#         hq.heapify(negValues)
+#         for i in range(k):
+#             poped=hq.heappop(negValues)
+#             output.append(-1*poped)
+        
+#     def negative(self,val):
+#         return -1*val
