4a401006f03272c38d41eae99b6cd0354c6123dc
Sefineh Tesfa <sefinehtesfa34@gmail.com>
2022-06-19 23:36:35 +0300

    simple arrays

---
+++ arrays.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+    arrrev=arr[::-1]
+    for i in arrrev:
+        print(i,end=" ")
