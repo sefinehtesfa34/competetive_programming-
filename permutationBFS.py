b4630739c13c537f0ee1b27a346d7074860226da
= <=>
2022-09-05 00:23:48 +0300

    Permutations using BFS

---
+++ permutationBFS.py
@@ -0,0 +1,23 @@
+from collections import deque
+def find_permutations(nums):
+  result = deque([[]])
+  for num in nums:
+    size = len(result)
+    for _ in range(size):
+      front = list(result.popleft())
+      for index in range(len(front) + 1):
+        temp = list(front)
+        temp.insert(index,num)
+        result.append(list(temp))
+  return result
+
+
+
+
+
+def main():
+  print("Here are all the permutations: " + str(find_permutations([1, 3,4, 5])))
+
+main()
+
+
