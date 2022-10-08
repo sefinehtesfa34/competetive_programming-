d6b6153445ebb09ae95cf7648adbfc133895e95d
= <=>
2022-10-08 23:14:51 +0300

    longest nice subarray

---
+++ longest_nice_subarray.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestNiceSubarray(self, nums: List[int]) -> int:
+        xor = left = longest = 0
+        for index in range(len(nums)):
+            while xor & nums[index]:
+                xor ^=  nums[left]
+                left += 1
+            xor ^= nums[index]
+            longest = max(longest, index - left + 1)
+        return longest
+    
+            
