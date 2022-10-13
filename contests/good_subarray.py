f1047062b147cce8386f6f1ab48ec602e51ad016
= <=>
2022-10-13 23:11:18 +0300

    good subarrays

--- contests/good_subarray.py
+++ contests/good_subarray.py
@@ -4,8 +4,8 @@ for _ in range(t):
     nums = list(map(int, input().split()))
     left = right = numSubarrays = 0
     while right < len(nums):
-        if nums[right] < right - left + 1:
-            left = right 
+        while nums[right] < right - left + 1:
+            left += 1
         numSubarrays += right - left + 1
         right += 1
     print(numSubarrays)
