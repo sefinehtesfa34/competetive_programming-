0c75b729c98585d6b816a12d6b136b36770c588e
Sefineh Tesfa <88246220+sefinehtesfa34@users.noreply.github.com>
2022-10-30 15:02:34 +0300

    Time: 894 ms (9.23%), Space: 16.5 MB (6.68%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -0,0 +1,44 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        # if n == 1 then return 0
+        # no price is needed
+        
+    
+        def build_graph(flights):
+            graph = defaultdict(list)
+            for start, end, price in flights:
+                graph[start].append((end, price))
+            return graph
+
+        def dfs(graph, sr, dst, k, memo = {}):
+            if (sr, dst, k) in memo:
+                return memo[(sr, dst, k)]
+            # if k is greater than or equal to zero and sr == dst, return the current minimum    
+            if sr == dst and k >= -1:
+                return 0
+            # if sr != dst and k <= 0, this is not a valid path
+            if k < 0:
+                return inf
+
+            #Find the minimum valid price
+            min_path = inf
+            for child in graph[sr]:
+                min_path = min(min_path, child[1] + dfs(graph, child[0], dst, k - 1))    
+
+            # add the current minimum into the cache
+            memo[(sr, dst, k)] = min_path
+            return memo[(sr, dst, k)]
+
+        # Build a graph
+        graph = build_graph(flights)  
+        #traverse the graph
+        result = dfs(graph, src, dst, k)
+        return result if result != inf else -1
+    
+        
+        
+        
+        
+        
+        
+        
