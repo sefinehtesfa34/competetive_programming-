709b1db3f5dba47e5a250a14d834e1ecbaeaae59
Sefineh Tesfa <88246220+sefinehtesfa34@users.noreply.github.com>
2022-10-30 15:04:00 +0300

    Time: 667 ms (21.56%), Space: 16.6 MB (6.41%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -11,8 +11,8 @@ class Solution:
             return graph
 
         def dfs(graph, sr, dst, k, memo = {}):
-            if (sr, dst, k) in memo:
-                return memo[(sr, dst, k)]
+            if (sr, k) in memo:
+                return memo[(sr, k)]
             # if k is greater than or equal to zero and sr == dst, return the current minimum    
             if sr == dst and k >= -1:
                 return 0
@@ -26,8 +26,8 @@ class Solution:
                 min_path = min(min_path, child[1] + dfs(graph, child[0], dst, k - 1))    
 
             # add the current minimum into the cache
-            memo[(sr, dst, k)] = min_path
-            return memo[(sr, dst, k)]
+            memo[(sr, k)] = min_path
+            return memo[(sr, k)]
 
         # Build a graph
         graph = build_graph(flights)  
