f4224ffb5615137c74077a4571b4ff9967c5613e
Sefineh Tesfa <88246220+sefinehtesfa34@users.noreply.github.com>
2022-11-12 16:19:57 +0300

    Time: 355 ms (63.86%), Space: 10.6 MB (30.76%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,19 @@
+using namespace std;
+class Solution {
+public:
+
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n + 1, 1);
+        for (int index = 1; index < n; index++){
+            for (int curr = 0; curr < index; curr++){
+                if (nums[index] > nums[curr]){
+                    dp[index] = max(dp[index], 1 + dp[curr]);
+                }
+            }
+        }
+          
+        return *max_element(dp.begin(), dp.end());
+        
+    }
+};
