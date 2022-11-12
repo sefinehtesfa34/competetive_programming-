9472e7e81e026dc89fb4960d4c87f884282287de
= <=>
2022-11-12 16:22:06 +0300

    longest increasing subsequence c++

--- longest_increasing_subsequence.cpp
+++ longest_increasing_subsequence.cpp
@@ -1,9 +1,9 @@
+using namespace std;
 class Solution {
 public:
-
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, 1);
+        vector<int> dp(n + 1, 1);
         for (int index = 1; index < n; index++){
             for (int curr = 0; curr < index; curr++){
                 if (nums[index] > nums[curr]){
@@ -11,7 +11,7 @@ public:
                 }
             }
         }
-        return dp[n];
+        return *max_element(dp.begin(), dp.end());
         
     }
 };
