3b3b43815ba983760881e994dace8780496a2585
Sefineh Tesfa <88246220+sefinehtesfa34@users.noreply.github.com>
2022-11-16 17:10:12 +0300

    Time: 3840 ms (26.88%), Space: 24.7 MB (81.89%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
@@ -1,28 +1,21 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        self.matrix=matrix
-        self.prefixSum=[[0]*(len(matrix[0])+1) for _ in range(len(matrix)+1)]
-        for row in range(len(matrix)):
-            for col in range(len(matrix[0])):
-                self.prefixSum[row+1][col+1]=matrix[row][col]\
-                                             + self.prefixSum[row][col+1]\
-                                             + self.prefixSum[row+1][col]\
-                                             - self.prefixSum[row][col]
+        self.dp = [[0]*(len(matrix[0]) + 1) for _ in range(len(matrix) + 1)]
+        for row in range(1, len(self.dp)):
+            for col in range(1, len(self.dp[0])):
+                self.dp[row][col]  = matrix[row - 1][col - 1] + \
+                                self.dp[row - 1][col] + \
+                                self.dp[row][col - 1] - \
+                                self.dp[row - 1][col - 1]
         
+            
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        return self.prefixSum[row2+1][col2+1] \
-                           - self.prefixSum[row2+1][col1]\
-                           - self.prefixSum[row1][col2+1]\
-                           + self.prefixSum[row1][col1]
+        
+        result = self.dp[row2 + 1][col2 + 1] - self.dp[row2 + 1][col1] - \
+                 self.dp[row1][col2 + 1] + self.dp[row1][col1]
+        return result
     
-        # [[0, 0, 0, 0, 0, 0], 
-        # [0, 3, 3, 4, 8, 10], 
-        # [0, 8, 14, 18, 24, 27], 
-        # [0, 9, 17, 21, 28, 36], 
-        # [0, 13, 22, 26, 34, 49], 
-        # [0, 14, 23, 30, 38, 58]]
-
 
 
 # Your NumMatrix object will be instantiated and called as such:
