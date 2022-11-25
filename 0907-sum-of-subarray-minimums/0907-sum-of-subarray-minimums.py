e20a11fe54700c6c42b0007cb77dbb7ce6691e2a
Sefineh Tesfa <88246220+sefinehtesfa34@users.noreply.github.com>
2022-11-25 16:18:25 +0300

    Time: 2220 ms (5.02%), Space: 23.1 MB (5.83%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
@@ -10,27 +10,21 @@ class Solution:
                     cur_pos = stack.pop()
                     contributions[cur_pos][pos] = abs(index - cur_pos)
                 stack.append(index)
-            
+                
             for remain in stack:
                 contributions[remain][pos] = abs(stack[-1] - remain) + 1
-                
         monotonic(arr, 1, 0, len(arr), 1)
         monotonic(arr, 0, len(arr) - 1, -1, -1)
         result = 0
-        
         for index in contributions:
             result += arr[index] * contributions[index][0] * contributions[index][1]
-            result %= (10 ** 9 + 7)
         return result % (10 ** 9 + 7)
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-            
+    
+    
+    
+    
+    
+    
+    
+    
+    
