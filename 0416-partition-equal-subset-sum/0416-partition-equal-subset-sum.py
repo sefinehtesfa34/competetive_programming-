29a10da795096805c2da32be5bb838d368931be9
Sefineh Tesfa <88246220+sefinehtesfa34@users.noreply.github.com>
2022-12-04 22:17:00 +0300

    Time: 9336 ms (5.02%), Space: 29.4 MB (42.32%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -5,9 +5,8 @@ class Solution:
             return False
         target_sum =  total_sum // 2
         dp = [[0]*(target_sum + 1) for _ in range(len(nums) + 1)]
-        for index in range(len(nums)):
-            dp[index][0] = True
         for row in range(1, len(nums) + 1):
+            dp[row][0] = True
             for target in range(1, target_sum + 1):
                 if target >= nums[row - 1]:
                     dp[row][target] = dp[row - 1][target - nums[row - 1]] or dp[row - 1][target]
