ca434df2fad365d2893089c3fa2bf84d61324bba
Sefineh Tesfa <88246220+sefinehtesfa34@users.noreply.github.com>
2022-12-07 00:25:36 +0300

    Time: 414 ms (38.34%), Space: 14.9 MB (98.74%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,26 +1,15 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        graph = defaultdict(list)
-        dp = [[0]*k for _ in range(n)]
-        memo = {}
-        for fro, to, cost in flights:
-            graph[fro].append((to, cost))
-        memo = {}
-        def dp(src, k):
-            if (src, k) in memo:
-                return memo[src, k]
+        #Bellman ford algorigthm
+        dp = [inf]*n
+        dp[src] = 0
+        for _ in range(k + 1):
+            temp_dp = dp.copy()
+            for fro, to, cost in flights:
+                if dp[fro] + cost < temp_dp[to]:
+                    temp_dp[to] = cost + dp[fro]
+            dp = temp_dp
             
-            if src == dst and k >= -1:
-                return 0
-            if k <= -1:
-                return inf
-            min_cost = inf
-            for child, cost in graph[src]:
-                min_cost = min(min_cost, cost + dp(child, k - 1))
-            memo[src, k] =  min_cost
-            return min_cost
-        result = dp(src, k)
-        return result if result != inf else -1
+        return dp[dst] if dp[dst] != inf else -1
+    
             
-            
-        
