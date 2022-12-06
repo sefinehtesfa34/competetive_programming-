e231981d22def64af212f8e9d448380c71aefc0e
Sefineh Tesfa <88246220+sefinehtesfa34@users.noreply.github.com>
2022-12-06 23:49:30 +0300

    Time: 300 ms (49.35%), Space: 16.8 MB (5.27%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,44 +1,26 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        # if n == 1 then return 0
-        # no price is needed
-        
-    
-        def build_graph(flights):
-            graph = defaultdict(list)
-            for start, end, price in flights:
-                graph[start].append((end, price))
-            return graph
-
-        def dfs(graph, sr, dst, k, memo = {}):
-            if (sr, k) in memo:
-                return memo[(sr, k)]
-            # if k is greater than or equal to zero and sr == dst, return the current minimum    
-            if sr == dst and k >= -1:
+        graph = defaultdict(list)
+        dp = [[0]*k for _ in range(n)]
+        memo = {}
+        for fro, to, cost in flights:
+            graph[fro].append((to, cost))
+        memo = {}
+        def dp(src, k):
+            if (src, k) in memo:
+                return memo[src, k]
+            
+            if src == dst and k >= -1:
                 return 0
-            # if sr != dst and k <= 0, this is not a valid path
-            if k < 0:
+            if k <= -1:
                 return inf
-
-            #Find the minimum valid price
-            min_path = inf
-            for child in graph[sr]:
-                min_path = min(min_path, child[1] + dfs(graph, child[0], dst, k - 1))    
-
-            # add the current minimum into the cache
-            memo[(sr, k)] = min_path
-            return memo[(sr, k)]
-
-        # Build a graph
-        graph = build_graph(flights)  
-        #traverse the graph
-        result = dfs(graph, src, dst, k)
+            min_cost = inf
+            for child, cost in graph[src]:
+                min_cost = min(min_cost, cost + dp(child, k - 1))
+            memo[src, k] =  min_cost
+            return min_cost
+        result = dp(src, k)
         return result if result != inf else -1
-    
-        
-        
-        
-        
-        
-        
+            
+            
         
