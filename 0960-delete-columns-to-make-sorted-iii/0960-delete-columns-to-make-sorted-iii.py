d8a1033e8cf5f15f03688e052258bfd9ba4139f5
Sefineh Tesfa <88246220+sefinehtesfa34@users.noreply.github.com>
2023-01-09 10:44:26 +0300

    Time: 198 ms (81.40%), Space: 13.9 MB (90.70%) - LeetHub

--- 0960-delete-columns-to-make-sorted-iii/0960-delete-columns-to-make-sorted-iii.py
+++ 0960-delete-columns-to-make-sorted-iii/0960-delete-columns-to-make-sorted-iii.py
@@ -1,18 +1,10 @@
 class Solution:
     def minDeletionSize(self, strs: List[str]) -> int:
-        for index in range(len(strs)):
-            strs[index] = " " + strs[index]
-        answer = 0
+        dp = [1] * len(strs[0])
         n = len(strs[0])
-        @cache
-        def dp(index, prev):
-            if index == n:
-                return 0
-            is_valid = True
-            if not all(word[prev] <= word[index] for word in strs):
-                is_valid = False
-            pick = dp(index + 1, index) if is_valid else inf
-            notPick = 1 + dp(index + 1, prev) 
-            return min(pick, notPick)
-        return dp(0, 0)
+        for left in range(n - 2, -1, -1):
+            for right in range(left + 1, n):
+                if all(word[left] <= word[right] for word in strs):
+                    dp[left] = max(dp[left], 1 + dp[right])
+        return n - max(dp)
     
