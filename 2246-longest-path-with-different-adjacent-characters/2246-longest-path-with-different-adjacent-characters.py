3e14be2052f701e3039d06ee8a94dc29cb9216c5
Sefineh Tesfa <88246220+sefinehtesfa34@users.noreply.github.com>
2023-01-13 09:49:12 +0300

    Time: 2079 ms (76.32%), Space: 154.7 MB (38.46%) - LeetHub

---
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
@@ -0,0 +1,25 @@
+class Solution:
+    def longestPath(self, parent: List[int], s: str) -> int:
+        graph = defaultdict(list)
+        self.max = 0
+        for node in range(len(parent)):
+            graph[parent[node]].append(node)
+        def dfs(current, par):
+            first_max = second_max = 0
+            for child in graph[current]:
+                if child == par:
+                    continue
+                cur_max = dfs(child, current)
+                if s[child] == s[current]:
+                    self.max = max(self.max, cur_max)
+                    continue
+                second_max = max(second_max, min(first_max, cur_max))
+                first_max = max(first_max, cur_max)
+            self.max = max(self.max, 1 + first_max + second_max)
+            return 1 + first_max
+        dfs(0, -1)
+        return self.max
+        
+        
+        
+            
