341a1931172e48f2425a152377aa620f097a530d
Sefineh Tesfa <88246220+sefinehtesfa34@users.noreply.github.com>
2023-01-18 22:25:11 +0300

    Time: 546 ms (81.95%), Space: 18.7 MB (98.34%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
@@ -3,15 +3,12 @@ class Solution:
         prefix = [0]*(len(nums))
         prefix[0] = nums[0]
         dp = [0]*len(nums)
-        cur_sum = -inf
-        best_sum = -inf
-        max_pref = -inf
+        cur_sum = best_sum = max_pref = -inf
         for index in range(1, len(nums)):
             prefix[index] = prefix[index - 1] + nums[index]
         for index in range(len(nums)):
             cur_sum = max(nums[index], cur_sum + nums[index])
-            best_sum = max(best_sum, cur_sum)
-            best_sum = max(best_sum, max_pref + prefix[-1] - prefix[index])
+            best_sum = max(best_sum, cur_sum, max_pref + prefix[-1] - prefix[index])
             max_pref = max(max_pref, prefix[index])
         return best_sum
     
