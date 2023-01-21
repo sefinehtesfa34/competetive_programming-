db05decfdd4b03c0f84c370987c995fdfcb6f064
Sefineh Tesfa <88246220+sefinehtesfa34@users.noreply.github.com>
2023-01-21 23:01:36 +0300

    Time: 3415 ms (31.75%), Space: 30.6 MB (65.84%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -1,18 +1,18 @@
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
-        cur_max = 1
         dp = [[0]*(len(s) + 1) for _ in range(len(s) + 1)]
-        for left in range(1, len(s) + 1):
-            dp[left][left] = 1
-            for right in range(left - 1 , 0, -1):
-                if s[left - 1] == s[right - 1]:
+        for index in range(len(s)):
+            dp[index][index] = 1
+        max_len = 1
+        for left in range(len(s)):
+            for right in range(len(s)-1, -1, -1):
+                if right < left:
+                    break
+                if right == left:
+                    dp[left][right] = 1 + dp[left - 1][right + 1]
+                elif s[left] == s[right]:
                     dp[left][right] = 2 + dp[left - 1][right + 1]
                 else:
-                    dp[left][right] = max(dp[left][right + 1], dp[left - 1][right])
-                cur_max = max(dp[left][right], cur_max)
-        return cur_max
-    
-    
-    
-    
-    
+                    dp[left][right] = max(dp[left - 1][right], dp[left][right + 1])
+                max_len = max(max_len, dp[left][right])
+        return max_len
