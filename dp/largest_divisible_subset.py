87a7c07fa74bc0bc21fb80441a852e2238dbb3e0
= <=>
2023-01-26 08:49:02 +0300

    largest divisible subset

--- dp/largest_divisible_subset.py
+++ dp/largest_divisible_subset.py
@@ -1,37 +1,22 @@
-from math import lcm
-
-
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
         nums.sort()
-        dp =[[0, 0] for _ in range(len(nums))]
-        cur_index = cur_max = 0
-        for index in range(len(nums)):
-            dp[index][1] = nums[index]
-            dp[index][0] = 1
+        dp = [1]*len(nums)
+        answer = []
         for index in range(len(nums)):
             for left in range(index):
-                if dp[index][0] <= dp[left][0] and nums[index] % dp[left][1] == 0:
-                    dp[index][0] = 1 + dp[left][0]
-                    dp[index][1] = lcm(dp[left][1], nums[index]) 
-                if dp[index][0] > cur_max:
-                    cur_index = index
-                    cur_max = dp[index][0]
-        answer = []
-        while cur_index >= 0:
-            answer.append(nums[cur_index])
-            shift_pos = cur_index - 1
-            if dp[cur_index][0] == 1:
+                if nums[index] % nums[left] == 0:
+                    dp[index] = max(dp[index], 1 + dp[left])
+        index = dp.index(max(dp))
+        while index >= 0:
+            answer.append(nums[index])
+            shift_index = index - 1
+            max_len = 0
+            if dp[index] == 1:
                 return answer
-            cur_len = 0
-            for index in range(cur_index):
-                if lcm(dp[index][1], nums[cur_index]) == dp[cur_index][1]:
-                    if dp[index][0] > cur_len:
-                        cur_len = dp[index][0]
-                        shift_pos = index
-            cur_index = shift_pos
+            for left in range(index):
+                if nums[index] % nums[left] == 0 and dp[left] > max_len:
+                    max_len = dp[left]
+                    shift_index = left
+            index = shift_index
         return answer
-    
-    
-    
-        
