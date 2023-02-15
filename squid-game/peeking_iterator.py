3322c4427d55123c811145e8d935377651c97ad8
Sefineh Tesfa <sefinehtesfa34@gmail.com>
2023-02-15 20:34:24 +0300

    squid game

---
+++ squid-game/peeking_iterator.py
@@ -0,0 +1,43 @@
+# Below is the interface for Iterator, which is already defined for you.
+#
+# class Iterator:
+#     def __init__(self, nums):
+#         """
+#         Initializes an iterator object to the beginning of a list.
+#         :type nums: List[int]
+#         """
+#
+#     def hasNext(self):
+#         """
+#         Returns true if the iteration has more elements.
+#         :rtype: bool
+#         """
+#
+#     def next(self):
+#         """
+#         Returns the next element in the iteration.
+#         :rtype: int
+#         """
+
+class PeekingIterator:
+    def __init__(self, iterator):
+        self.iterator = iterator
+        self.buffer = self.iterator.next() if self.iterator.hasNext() else None
+        
+    def peek(self):
+        return self.buffer
+        
+    def next(self):
+        tmp = self.buffer
+        self.buffer = self.iterator.next() if self.iterator.hasNext() else None
+        return tmp
+        
+    def hasNext(self):
+        return self.buffer != None
+        
+
+# Your PeekingIterator object will be instantiated and called as such:
+# iter = PeekingIterator(Iterator(nums))
+# while iter.hasNext():
+#     val = iter.peek()   # Get the next element but not advance the iterator.
+#     iter.next()         # Should return the same value as [val].
