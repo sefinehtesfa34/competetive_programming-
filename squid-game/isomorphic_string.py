622b6492aa54ffc58693b5001d0aa536a0dab4a7
Sefineh Tesfa <sefinehtesfa34@gmail.com>
2023-02-22 22:34:56 +0300

    squid game

---
+++ squid-game/isomorphic_string.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        
+        mapping_s_t = {}
+        mapping_t_s = {}
+        
+        for c1, c2 in zip(s, t):
+            if (c1 not in mapping_s_t) and (c2 not in mapping_t_s):
+                mapping_s_t[c1] = c2
+                mapping_t_s[c2] = c1           
+            elif mapping_s_t.get(c1) != c2 or mapping_t_s.get(c2) != c1:
+                return False
+            
+        return True
